294a8b702dfff8acdceb1a338fd496d021e39333
blukatstudios <blukatstudios@gmail.com>
2018-04-03 12:39:51 -0400

    Added new item scenes.

--- Items/Item.cs
+++ Items/Item.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Reflection;
 
 
-public class Item : Node {
+public class Item : RigidBody {
 	public enum Uses{ A, B, C, D, E, F, G };
-
-
-
+  
+  public int quantity;
+  public int quantityMax;  
+  
 	public static Node Factory(string itemName){
 		Node ret = null;
 		switch(itemName.ToLower()){
@@ -16,5 +17,7 @@ public class Item : Node {
 		}
 		return ret;
 	}
+	
+	
 
 }
