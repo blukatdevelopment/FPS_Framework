588642cabd1c5fbfbec5d98e11ad186c73e45946
blukatstudios <blukatstudios@gmail.com>
2018-05-02 23:11:41 -0400

    Added event file

---
+++ Util/Events/SessionEvent.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class SessionEvent {
+  public Actor[] actors;
+  public Types type;
+  public enum Types { None, ActorDied };
+  
+  public SessionEvent() {
+    this.type = Types.None;
+    this.actors = new Actor[0]; 
+  }
+}
