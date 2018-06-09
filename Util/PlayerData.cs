a6822986461510adc30a5f68822b1750a1269df9
blukatstudios <blukatstudios@gmail.com>
2018-06-08 22:45:38 -0400

    Adding playerData back to NetworkSession

---
+++ Util/PlayerData.cs
@@ -0,0 +1,14 @@
+/*
+  A bundle of information for a networked player.
+*/
+public class PlayerData {
+  public string name;
+  public int id;
+  public bool ready;
+
+  public PlayerData(string name, int id){
+    this.name = name;
+    this.id = id;
+    this.ready = false;
+  }
+}
